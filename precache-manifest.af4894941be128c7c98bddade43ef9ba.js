4ca5b8a9dd60ae609338f941c67953030e3b7acf
manovijay1997 <jobinijohnson981@gmail.com>
2020-04-17 22:30:10 +0530

    Updates

---
+++ precache-manifest.af4894941be128c7c98bddade43ef9ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed74fe04e4716e0444597bce017652b",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "ffdf736cee0aae4c4c4d",
+    "url": "/To-Do-App/static/css/main.fd52993f.chunk.css"
+  },
+  {
+    "revision": "0fd034b0787430342756",
+    "url": "/To-Do-App/static/js/2.5b6c0a03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App/static/js/2.5b6c0a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffdf736cee0aae4c4c4d",
+    "url": "/To-Do-App/static/js/main.3ae15c42.chunk.js"
+  },
+  {
+    "revision": "02460c2875bb1c6530e1",
+    "url": "/To-Do-App/static/js/runtime-main.8a04a3bb.js"
+  }
+]);
